770e2bfb35b323d9659c94793bfaaf2384a85850
120L020104 <fan_hanlin@163.com>
2022-05-31 18:44:02 +0800

    first

---
+++ src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		for(int i = 0; i<1;i++) {int j = 0;}
+		j = 1;
+	}
+
+}
